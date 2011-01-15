abdf2e06a42e8b630fb57adbb852a7c7617a54d3
Andre Pankratz <andre@webwarelab.com>
2011-01-15 16:58:01 +0100

    Added spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
+
+require "rubygems"
+require "rspec"
+require "rack/test"
+require "rr"
+require "webmock/rspec"
+require "vidibus-resource"
+
+Mongoid.configure do |config|
+  name = "vidibus-resource_test"
+  host = "localhost"
+  config.master = Mongo::Connection.new.db(name)
+  config.logger = nil
+end
+
+RSpec.configure do |config|
+  config.include WebMock
+  config.mock_with :rr
+  config.before(:each) do
+    Mongoid.master.collections.select {|c| c.name !~ /system/}.each(&:drop)
+  end
+end
