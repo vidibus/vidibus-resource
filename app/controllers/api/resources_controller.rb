d29b109fa8ddf0e214dfaf5c2ecf4a947c661734
Andre Pankratz <andre@webwarelab.com>
2011-09-13 20:36:20 +0200

    Refactor resources controller

--- app/controllers/api/resources_controller.rb
+++ app/controllers/api/resources_controller.rb
@@ -1,18 +1,18 @@
 # TODO: Split this file: one part for providers, one for consumers!
 class Api::ResourcesController < ApiController
   before_filter :ensure_klass
-  before_filter :find_instance
+  before_filter :ensure_instance
 
   # Creates resource consumer on provider.
   def create
-    @instance.add_resource_consumer(params['service'], params['realm'])
-    render(:json => {:resource => JSON.generate(@instance.resourceable_hash)})
+    instance.add_resource_consumer(params[:service], params[:realm])
+    render(:json => {:resource => JSON.generate(instance.resourceable_hash)})
   end
 
   # Updates resource on consumer.
   def update
     begin
-      @instance.update_resource_attributes(params['resource'])
+      instance.update_resource_attributes(params['resource'])
       render :nothing => true
     rescue => e
       Rails.logger.error "Error while updating resource consumer:\n#{e.inspect}"
@@ -22,29 +22,55 @@ class Api::ResourcesController < ApiController
 
   # Removes a resource consumer from provider or consumer.
   def destroy
-    if @instance.respond_to?(:resource_consumers)
-      @instance.remove_resource_consumer(params['service'], params['realm'])
+    if provider?
+      instance.remove_resource_consumer(params[:service], params[:realm])
     else
-      @instance.destroy_without_callback
+      instance.destroy_without_callback
     end
-    render :nothing => true
+    render(:nothing => true)
   end
 
   private
 
-  def ensure_klass
-    begin
-      @klass = params[:klass].classify.constantize
+  def klass
+    @klass ||= begin
+      params[:klass].classify.constantize
     rescue => e
-      render :json => {:error => e}, :status => :bad_request
+      @klass_error = e
     end
   end
 
-  def find_instance
-    result = @klass.where(:uuid => params[:uuid])
-    if @klass.new.respond_to?(:realm_uuid)
-      result = result.and(:realm_uuid => params[:realm])
+  def klass_with_realm?
+    @is_klass_with_realm ||= klass.instance_methods.include?('realm_uuid')
+  end
+
+  def instance
+    @instance ||= begin
+      results = klass.where(:uuid => params[:uuid])
+      if klass_with_realm?
+        results = results.and(:realm_uuid => params[:realm])
+      end
+      results.first
     end
-    @instance = result.first or render(:json => {:error => "#{@klass} #{params[:uuid]} not found"}, :status => :not_found)
+  end
+
+  def no_instance_required?
+    %w[create].include?(action_name) and !provider?
+  end
+
+  def ensure_klass
+    if !klass or @klass_error
+      render(:json => {:error => @klass_error}, :status => :bad_request)
+    end
+  end
+
+  def provider?
+    @is_provider ||= klass.instance_methods.include?('resource_consumers')
+  end
+
+  def ensure_instance
+    unless instance
+      render(:json => {:error => "#{klass} #{params[:uuid]} not found"}, :status => :not_found)
+    end
   end
 end
