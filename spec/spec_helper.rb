28be091b9662f648135539fd82782e0de101b314
Andre Pankratz <andre@webwarelab.com>
2011-09-13 13:01:36 +0200

    Cleanup spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,23 +1,20 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
+require 'rspec'
+require 'mongoid'
+require 'rack/test'
+require 'rr'
+require 'webmock/rspec'
+require 'vidibus-resource'
 
-require "rubygems"
-require "rspec"
-require "mongoid"
-require "rack/test"
-require "rr"
-# require "webmock/rspec"
-require "vidibus-resource"
 
 Mongoid.configure do |config|
-  name = "vidibus-resource_test"
-  host = "localhost"
+  name = 'vidibus-resource_test'
+  host = 'localhost'
+  # config.master = Mongo::Connection.new("localhost", 27017, :logger => Logger.new($stdout, :info)).db(name)
   config.master = Mongo::Connection.new.db(name)
   config.logger = nil
 end
 
 RSpec.configure do |config|
-  # config.include WebMock
   config.mock_with :rr
   config.before(:each) do
     Mongoid.master.collections.select {|c| c.name !~ /system/}.each(&:drop)
